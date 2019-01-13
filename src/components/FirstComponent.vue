73be90ade0e4afb84b0b09ded2a573a0cfa66a68
Ian Goldfarb <ian@supplyhub.com>
2019-01-13 12:07:19 -0800

    created my first vue component as a test run

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="first">
+    <div>Hello World, this is my first Vue Component</div>
+    <div>We shall now pass a property: <span class="test">{{ test }}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent',
+  props: {
+    test: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .first {
+    font-size: 16px;
+
+    .test {
+      background-color: red;
+    }
+  }
+</style>
