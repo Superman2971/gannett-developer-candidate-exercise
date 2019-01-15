a9aa9639b14a6fc46d1e07719aaa7d279cdddfab
Ian Goldfarb <ian@supplyhub.com>
2019-01-14 18:14:43 -0800

    removed axios and installed vue-jsonp for requests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueJsonp from 'vue-jsonp'
+Vue.use(VueJsonp)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,6 @@
 <script>
 // @ is an alias to /src
 import Article from '@/components/Article.vue'
-import axios from 'axios'
 
 export default {
   name: 'home',
@@ -47,11 +46,9 @@ export default {
       console.log('Cookie GET', cookieProfile)
       if (cookieProfile) {
         this.profile = JSON.parse(cookieProfile)
-        // this.getArticles()
-        this.getFakedArticles()
+        this.getArticles()
       } else {
-        // this.getProfile()
-        this.getFakeProfile()
+        this.getProfile()
       }
     },
     getCookie: function (name) {
@@ -65,7 +62,7 @@ export default {
       return null
     },
     getProfile: function () {
-      axios.get('https://peaceful-springs-7920.herokuapp.com/profile/')
+      this.$jsonp('https://peaceful-springs-7920.herokuapp.com/profile/')
         .then((response) => {
           console.log('success', response)
           this.profile = response
@@ -75,13 +72,6 @@ export default {
           console.log('error', error)
           this.loading = false
         })
-    },
-    getFakeProfile: function () {
-      this.profile = {
-        'profileId': 3
-      }
-      this.setCookie('profile', JSON.stringify(this.profile), 365)
-      this.getFakedArticles()
     },
     setCookie: function (name, value, days) {
       var expires = ''
@@ -94,7 +84,7 @@ export default {
       document.cookie = name + '=' + (value || '') + expires + '; path=/'
     },
     getArticles: function () {
-      axios.get(`https://peaceful-springs-7920.herokuapp.com/content/${this.profile['profileId']}`)
+      this.$jsonp(`https://peaceful-springs-7920.herokuapp.com/content/${this.profile.profileId}`)
         .then((response) => {
           console.log('success', response)
           this.articles = response.articles
@@ -104,39 +94,6 @@ export default {
           console.log('error', error)
           this.loading = false
         })
-    },
-    getFakedArticles: function () {
-      setTimeout(() => {
-        this.articles = [
-          {
-            'href': 'http://example.com/3/0/',
-            'summary': 'Shank prosciutto tail meatball, pancetta sirloin rump shoulder ham bacon picanha chuck jowl swine. Strip steak porchetta brisket tail, cow cupim boudin ham hock bacon shoulder hamburger bresaola t-bone. Tongue bresaola jerky meatloaf. Porchetta meatloaf jowl, brisket drumstick kielbasa shank cupim flank fatback frankfurter prosciutto kevin.',
-            'title': 'Meatball'
-          },
-          {
-            'href': 'http://example.com/3/1/',
-            'summary': 'Bacon ipsum dolor amet spare ribs brisket bresaola porchetta, shankle shank jowl biltong swine. Shank pork chop kevin pork belly frankfurter salami. Short loin ball tip turducken, sirloin flank hamburger beef ribs andouille turkey. Short loin sausage chicken, tail brisket alcatra prosciutto. Short ribs pork belly strip steak pork, ribeye fatback andouille landjaeger. Pig frankfurter ball tip, sausage pork chop chuck tri-tip prosciutto. Jowl swine shank strip steak pork bresaola.',
-            'title': 'Bacon'
-          },
-          {
-            'href': 'http://example.com/3/2/',
-            'summary': 'Rump spare ribs corned beef prosciutto beef, biltong pork belly jerky strip steak meatloaf tenderloin tail ground round. Chicken short loin sirloin ground round pork belly meatball alcatra pork chop bacon filet mignon capicola. Pancetta jowl tenderloin spare ribs rump corned beef. Bacon jowl meatball capicola. Doner turkey landjaeger venison, shoulder pastrami ribeye jerky sausage hamburger.',
-            'title': 'Rump'
-          },
-          {
-            'href': 'http://example.com/3/3/',
-            'summary': 'Tongue short loin pork chop porchetta, kielbasa cow salami bresaola venison turkey fatback. Tail shoulder frankfurter pancetta flank bacon jerky porchetta fatback short loin meatloaf filet mignon pork chop chicken brisket. Tri-tip chuck meatball prosciutto ball tip. Meatloaf t-bone landjaeger andouille ham hock fatback porchetta pig rump hamburger sirloin boudin bacon sausage meatball. Sirloin ball tip biltong, shankle beef short ribs tri-tip shoulder picanha jerky hamburger tenderloin pastrami ham hock.',
-            'title': 'Tongue'
-          },
-          {
-            'href': 'http://example.com/3/4/',
-            'summary': 'Pancetta bresaola pig spare ribs picanha meatball andouille tri-tip leberkas turducken pork belly meatloaf shankle. Short loin short ribs turducken pork pork belly, cow beef ribs drumstick swine prosciutto salami sirloin. Bresaola sirloin cow pork, pig ham hock beef. Flank shank venison, shankle drumstick t-bone swine biltong sausage tail prosciutto porchetta fatback spare ribs. Chicken beef ribs fatback bacon. Cupim beef ribs spare ribs kielbasa venison shankle bresaola strip steak doner turkey meatloaf pastrami shank short ribs short loin. Venison bacon turkey beef fatback sirloin bresaola shoulder meatball alcatra ball tip picanha kielbasa corned beef capicola.',
-            'title': 'Pancetta'
-          }
-        ]
-        this.theme = 'well'
-        this.loading = false
-      }, 2000)
     }
   }
 }
